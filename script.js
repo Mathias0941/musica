15c3d8ce36874702c5137ec11763af0761bae03d
Mathias0941 <124325755+Mathias0941@users.noreply.github.com>
2023-02-02 20:16:58 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function cancion1(){
+    document.getElementById("audio1").play();
+    console.log("cancion1");
+}
+function cancion2(){
+    document.getElementById("audio2").play();
+}
+function cancion3(){
+    document.getElementById("audio3").play();
+}
+function cancion4(){
+    document.getElementById("audio4").play();
+}
